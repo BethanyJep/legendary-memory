553ef52108f55550571592f108b8035f21d26c19
Pikachú <engbethany4@gmail.com>
2023-08-08 09:43:29 +0000

    excercise 1 - 4 done, not really well though

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  function handleImageUrlChange(event) {
+    setImageUrl(event.target.value);
+  }
+
+  function handleAnalyzeClick() {
+    // TODO: implement analyzeImage function
+    console.log('Analyze Image button clicked');
+  }
+
+  function handleGenerateClick() {
+    // TODO: implement generateImage function
+    console.log('Generate Image button clicked');
+  }
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation</h1>
+
+      <label htmlFor="image-url">Image URL or prompt:</label>
+      <input type="text" id="image-url" name="image-url" value={imageUrl} onChange={handleImageUrlChange} />
+
+      <button onClick={handleAnalyzeClick}>Analyze Image</button>
+      <button onClick={handleGenerateClick}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
